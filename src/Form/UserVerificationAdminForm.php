58887ce71a0ea43a2797db17373cad3923656045
Sunil Pawar <sunil@skvare.com>
2021-09-16 16:27:49 +0530

    first commit

---
+++ src/Form/UserVerificationAdminForm.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Drupal\email_verification\Form;
+
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Class UserVerificationAdminForm.
+ *
+ * Admin setting form.
+ */
+class UserVerificationAdminForm extends ConfigFormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEditableConfigNames() {
+    return [
+      'email_verification.settings',
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'user_verification_admin_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $config = \Drupal::configFactory()->getEditable('email_verification.settings');
+    $form['user_email_verification_salt'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Random key to generate verify email hash'),
+      '#maxlength' => 64,
+      '#size' => 64,
+      '#weight' => '0',
+      '#default_value' => $config->get('user_email_verification_salt'),
+    ];
+    $form['user_email_verification_tpl'] = [
+      '#type' => 'textarea',
+      '#title' => $this->t('User Verification Email Template'),
+      '#weight' => '1',
+      '#default_value' => $config->get('user_email_verification_tpl'),
+      '#description' =>
+      $this->t('Set custom message for user email verification with these [user:emailverificationlink], [user:varifiedemail] tokens.'),
+    ];
+
+    return parent::buildForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    \Drupal::configFactory()->getEditable('email_verification.settings')
+      ->set('user_email_verification_salt', $form_state->getValue('user_email_verification_salt'))
+      ->set('user_email_verification_tpl', $form_state->getValue('user_email_verification_tpl'))
+      ->save();
+
+    return parent::submitForm($form, $form_state);
+  }
+
+}
---
+++ src/Form/UserVerificationForm.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace Drupal\email_verification\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Component\Utility\Html;
+
+/**
+ * Class UserVerificationForm.
+ *
+ * User Email functionality.
+ */
+class UserVerificationForm extends FormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'user_verification_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $form['mail'] = [
+      '#type' => 'email',
+      '#title' => $this->t('E-mail address'),
+      '#description' => $this->t('Validate Email address before creating account'),
+      '#weight' => '0',
+    ];
+    $form['submit'] = [
+      '#type' => 'submit',
+      '#value' => $this->t('Submit'),
+    ];
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    foreach ($form_state->getValues() as $key => $value) {
+      if ($key == 'mail') {
+        if (!\Drupal::service('email.validator')->isValid($value)) {
+          $form_state->setError($form['mail'], $this->t('Please provide valid email address.'));
+        }
+      }
+    }
+    parent::validateForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $values = $form_state->getValues();
+    $email = $values['mail'];
+    $config = \Drupal::configFactory()->getEditable('email_verification.settings');
+    $salt = $config->get('user_email_verification_salt') ?? 'email';
+    $emailKey = md5($salt . $email);
+    \Drupal::logger('email_verification-emailKey')->notice(print_r($emailKey, TRUE));
+    global $base_url;
+    // Get template.
+    $msgTpl = $config->get('user_email_verification_tpl');
+    // Prepare verification link.
+    $link = "{$base_url}/user/register?email={$email}&verify=" . $emailKey;
+    // Replace token.
+    $token_service = \Drupal::service('token');
+    $msgTpl = $token_service->replace($msgTpl,
+      ['varifiedemail' => $email, 'emailverificationlink' => $link]);
+    $module = 'email_verification';
+    \Drupal::logger('email_verification')->notice(print_r($msgTpl, TRUE));
+    // Send Email.
+    $mailManager = \Drupal::service('plugin.manager.mail');
+    // Replace with Your key.
+    $key = 'email_verification';
+    $to = $email;
+    $langcode = \Drupal::currentUser()->getPreferredLangcode();
+    $send = TRUE;
+    $params['message'] = $msgTpl;
+    $params['title'] = $this->t('User Email Verification');
+    ;
+    $params['from'] = \Drupal::config('system.site')->get('mail');
+    $params['subject'] = $this->t('User Email Verification');
+    $params['body'][] = Html::escape($params['message']);
+
+    $result = $mailManager->mail($module, $key, $to, $langcode, $params, NULL, $send);
+    if ($result['result'] != TRUE) {
+      $message =
+        $this->t('There was a problem sending your email notification to @email.',
+        ['@email' => $to]);
+      \Drupal::logger('email_verification')->error($message);
+
+      return;
+    }
+
+    $message = $this->t('An email notification has been sent to @email', ['@email' => $to]);
+    \Drupal::logger('email_verification')->notice($message);
+  }
+
+}
